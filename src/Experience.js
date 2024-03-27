1d2463c3153d19ef3c4920f17d92d81449d3d128
solaiman-naderi <solaimannaderi00@gmail.com>
2024-03-27 13:44:19 +0330

    added lights

--- src/Experience.js
+++ src/Experience.js
@@ -1,4 +1,10 @@
-import { useGLTF, OrbitControls, useTexture, Center } from "@react-three/drei";
+import {
+  Sparkles,
+  useGLTF,
+  OrbitControls,
+  useTexture,
+  Center,
+} from "@react-three/drei";
 
 export default function Experience() {
   const model = useGLTF("/model/portal.glb");
@@ -16,11 +22,24 @@ export default function Experience() {
         <mesh
           geometry={model.nodes.poleLightA.geometry}
           position={model.nodes.poleLightA.position}
-        />
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
         <mesh
           geometry={model.nodes.poleLightB.geometry}
           position={model.nodes.poleLightB.position}
-        />
+        >
+          <meshBasicMaterial color="#ffffe5" />
+        </mesh>
+
+        <mesh
+          geometry={model.nodes.portalLight.geometry}
+          position={model.nodes.portalLight.position}
+          rotation={model.nodes.portalLight.rotation}
+        >
+          <shaderMaterial />
+        </mesh>
+        <Sparkles size={6} scale={[4, 2, 4]} position-y={1} speed={0.5} />
       </Center>
       {/* <primitive object={model.scene} /> */}
     </>
